363ebc064b5d66c727008d08ac0a0d7d39facf2e
Erik M <eriksingh15@gmail.com>
2025-03-03 17:15:54 +0100

    Added uploading dialog to be shown after picking an image

--- app/src/main/java/com/welbo/app/ui/images/ImagesFragment.kt
+++ app/src/main/java/com/welbo/app/ui/images/ImagesFragment.kt
@@ -1,12 +1,15 @@
 package com.welbo.app.ui.images
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.welbo.app.databinding.FragmentImagesBinding
+import com.welbo.app.ui.images.upload.ImagesUploadFragment
 
 class ImagesFragment : Fragment() {
 
@@ -47,9 +50,24 @@ class ImagesFragment : Fragment() {
             putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpeg", "image/png"))
         }
 
-        // NOTE about technical design decision: going for deprecated solution because this one allows you to filter out images
+        // NOTE about technical choice here: going for deprecated solution because this one allows you to filter out images
         // with unsupported file extensions before they are picked. Also, robots run on older versions of Android, and newest
         // way of picking images on Android is from Android 13 and above.
-        startActivityForResult(intent, 1)
+        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK) {
+            val selectedImageUri: Uri? = data?.data
+
+            ImagesUploadFragment().show(childFragmentManager, ImagesUploadFragment.TAG)
+        }
+    }
+
+
+    companion object {
+        private const val REQUEST_CODE_PICK_IMAGE = 1
     }
 }
